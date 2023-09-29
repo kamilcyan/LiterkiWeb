26527763913e6aeb07c92d7d97760c495db3ad62
kamilcyan <kamil.lucjanek@wp.pl>
2023-09-29 21:28:18 +0200

    Controller works

---
+++ LiterkiWeb/LiterkiWeb/Controllers/LiterkiController.cs
@@ -0,0 +1,32 @@
+﻿using LiterkiWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiterkiWeb.Controllers
+{
+    public class LiterkiController : Controller
+    {
+        public IActionResult GetLetters()
+        {
+            List<Literki> literki = new List<Literki>();
+            literki.Add(new Literki()
+            {
+                Id = 1,
+                Nazwa = "A",
+                PictureUrl = "/images/aduze.png"
+            });
+            literki.Add(new Literki()
+            {
+                Id = 2,
+                Nazwa = "B",
+                PictureUrl = "/images/bduze.png"
+            });
+            literki.Add(new Literki()
+            {
+                Id = 3,
+                Nazwa = "C",
+                PictureUrl = "/images/cduze.png"
+            });
+            return View(literki);
+        }
+    }
+}
